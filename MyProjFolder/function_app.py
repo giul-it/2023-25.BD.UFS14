cb481354b0b33658d986f30bf7f8cffb88e463da
giul-it <giulio.salaorni@itsrizzoli.it>
2024-11-06 13:46:14 +0000

    change function app

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -2,28 +2,24 @@ import azure.functions as func
 import datetime
 import json
 import logging
-
-app = func.FunctionApp()
-
 from mongo_database import sperofunzioni
 
-
-
+app = func.FunctionApp()
 @app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    num = req.params.get("num")
-    if not num:
+    name = req.params.get('name')
+    if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get("num")
+            name = req_body.get('name')
 
-    if num:
-        return func.HttpResponse(f"{sperofunzioni(num)}")
+    if name:
+        return func.HttpResponse(f"Hello, {sperofunzioni(int(name))}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
